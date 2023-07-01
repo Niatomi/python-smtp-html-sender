c4aee61f1f38504dfa7615d28db91eb755a07930
Niatomi <playervoker@gmail.com>
2023-07-02 01:42:47 +0400

    changed config to more flexible way

--- src/__main__.py
+++ src/__main__.py
@@ -2,7 +2,18 @@ from .smtp import EmailSender
 
 def main():
     es = EmailSender()
-    es.send_email(to='playervoker@gmail.com', subject='Тестовое сообщение')
+    data = [
+    'Данные для авторизации в системе',
+    'Имя пользователя: niatomi',
+    'Пароль: waiodfjg',
+    'Подразделение: Самарский ИВЦ',
+    'Должность: Программист 1-й категории',
+    'Роль в системе: Администратор',
+    'Срок действия учётной записи: 12.12.2030'
+    ]
+    es.send_email(to='playervoker@gmail.com',
+                  subject='АС ВИДЕОКОНТРОЛЬ: Изменение учётной записи',
+                  data_content=data)
 
 if __name__ == "__main__":
     main()
